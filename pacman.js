fdb896db5f3a59af0af8ca18cd5ad6a86688ac0a
sadksafwiakdlakdwkda <zcc2423063428@163.com>
2024-01-29 00:00:14 -0500

    moveLeft moveRight created

--- pacman.js
+++ pacman.js
@@ -1,11 +1,28 @@
 function createGame(n){
-    let list = ["C","^","@"];
+    let game = ["C","^","@"];
     for(let i = 0; i<n-3;i++){
-        list.push(".")
+        game.push(".")
     }
-    for (let i = list.length - 1; i > 0; i--) {
+    for (let i = game.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [list[i], list[j]] = [list[j], list[i]];
+        [game[i], game[j]] = [game[j], game[i]];
     }
-    return list;
+    return game;
+}
+function moveLeft(game) {
+    let pacmanIndex = game.indexOf("C");
+    if (pacmanIndex > 0) {
+        game[pacmanIndex] = game[pacmanIndex-1];
+        game[pacmanIndex - 1] = "C";
+    }
+    return game;
+}
+
+function moveRight(game) {
+    let pacmanIndex = game.indexOf("C");
+    if (pacmanIndex < game.length - 1) {
+        game[pacmanIndex] = game[pacmanIndex+1];
+        game[pacmanIndex + 1] = "C";
+    }
+    return game;
 }
